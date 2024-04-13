629c95be678ad7c40c40a6034b23048eaa55fce5
손성호 <geronest@gmail.com>
2024-04-13 18:12:03 +0100

    ADD src/token_trace/utils.py: histogram_features()

--- src/token_trace/utils.py
+++ src/token_trace/utils.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from typing import Any
 
+import numpy as np
 
 def dump_jsonl(filepath: str | Path, objs: list[Any]):
     with open(filepath, "w") as f:
@@ -16,3 +17,30 @@ def load_jsonl(filepath: str | Path) -> list[Any]:
         for line in f:
             objs.append(json.loads(line))
     return objs
+
+def summary_features(data: list[Any]) -> dict:
+    pass
+
+def histogram_features(data: list[Any]) -> list:
+    """
+        used for a single block.
+    """
+    feat_ids = [int(item[0]) for item in data]
+    feat_values = [float(item[1]) for item in data]
+
+    v_min = np.floor(min(feat_values))
+    v_max = np.ceil(max(feat_values))
+    n_bins = int(v_max - v_min)
+
+    feat_locations = dict()
+    for i in range(len(feat_ids)):
+        feat_locations[feat_ids[i]] = np.floor(feat_values[i])
+
+    return [
+        np.histogram(
+            feat_values, 
+            range=(v_min, v_max), 
+            bins=n_bins
+        ),
+        feat_locations
+    ]
