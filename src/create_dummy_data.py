7ef2e140058925dfaae994d38d8958a7a48855ae
Daniel Tan <dtch1997@users.noreply.github.com>
2024-04-13 15:02:10 +0100

    Add pre-commit hooks

--- src/create_dummy_data.py
+++ src/create_dummy_data.py
@@ -1,24 +1,26 @@
-import transformers
 import pickle
-import json
-import torch
 import random
-
 from types import SimpleNamespace
-from sae_lens.training.utils import BackwardsCompatibleUnpickler
-from sae_lens import SparseAutoencoder
+
+import torch
+import transformers
 from huggingface_hub import hf_hub_download
-from token_trace.utils import dump_jsonl, load_jsonl
+from sae_lens import SparseAutoencoder
+from sae_lens.training.utils import BackwardsCompatibleUnpickler
+from token_trace.utils import dump_jsonl
 
 # Hardcoded
 model_name = "openai-community/gpt2"
 prompt = "I am a human"
 n_layers = 12
-n_sae_features = 100 # 24576
+n_sae_features = 100  # 24576
+
 
 def load_sae(layer: int) -> SparseAutoencoder:
     REPO_ID = "jbloom/GPT2-Small-SAEs"
-    FILENAME = f"final_sparse_autoencoder_gpt2-small_blocks.{layer}.hook_resid_pre_24576.pt"
+    FILENAME = (
+        f"final_sparse_autoencoder_gpt2-small_blocks.{layer}.hook_resid_pre_24576.pt"
+    )
     path = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
     # Hacky way to get torch to unpickle an old version of SAELens model
     fake_pickle = SimpleNamespace()
@@ -29,8 +31,9 @@ def load_sae(layer: int) -> SparseAutoencoder:
     sparse_autoencoder.load_state_dict(data["state_dict"])
     return sparse_autoencoder
 
+
 if __name__ == "__main__":
-    # Load model and tokenizer 
+    # Load model and tokenizer
     model = transformers.AutoModel.from_pretrained(model_name)
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
 
@@ -38,7 +41,7 @@ if __name__ == "__main__":
     inputs = tokenizer(prompt, return_tensors="pt")
     token_ids = inputs["input_ids"]
     token_strs = tokenizer.convert_ids_to_tokens(token_ids[0])
-    
+
     # dummy_data: list of (token_str, layer, feature_index, feature_value)
     dummy_data = []
     for token_str in token_strs:
--- src/token_trace/utils.py
+++ src/token_trace/utils.py
@@ -1,17 +1,18 @@
 import json
-
-from typing import Any
 from pathlib import Path
+from typing import Any
+
 
 def dump_jsonl(filepath: str | Path, objs: list[Any]):
     with open(filepath, "w") as f:
         for entry in objs:
             json.dump(entry, f)
-            f.write('\n')
+            f.write("\n")
+
 
 def load_jsonl(filepath: str | Path) -> list[Any]:
     objs = []
-    with open(filepath, "r") as f:
+    with open(filepath) as f:
         for line in f:
             objs.append(json.loads(line))
     return objs
